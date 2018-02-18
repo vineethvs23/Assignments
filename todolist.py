f140b7cb68c8fc4dfc3b1e9850c37f1241323a4a
vineethvs23 <vineethvs2301@gmail.com>
2018-02-18 11:53:10 +0530

    moved dicts inside class

--- todolist.py
+++ todolist.py
@@ -1,12 +1,17 @@
-table={}
-done={}
+#table={}
+#done={}
 class ToDoList(object):
+	def __init__():
+		self.table={}
+		self.done={}
 	def ToDo(self):
 		self.name=input("\nEnter the name of the To Do list:")
 		table[self.name]=[]
+	
 	def add(self):
 		self.task=input("\nEnter the task to be added:")
 		table[self.name].append(self.task)	
+	
 	def Markdone(self):
 		self.find=input("\nEnter the task name:")
 		for i in table[self.name]:
